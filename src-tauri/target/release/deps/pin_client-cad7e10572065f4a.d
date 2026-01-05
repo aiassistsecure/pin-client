7c48998b169765807905e14c3f9a85807f49b311
root <root@vmi2886890.contaboserver.net>
2026-01-05 08:16:04 +0100

    built 1st PIN network GUI client

--- src-tauri/target/release/deps/pin_client-cad7e10572065f4a.d
+++ src-tauri/target/release/deps/pin_client-cad7e10572065f4a.d
@@ -1,8 +1,16 @@
-/root/pin-client/src-tauri/target/release/deps/pin_client-cad7e10572065f4a.d: src/main.rs src/websocket.rs src/keychain.rs src/ollama.rs
+/root/pin-client/src-tauri/target/release/deps/pin_client-cad7e10572065f4a.d: src/main.rs src/websocket.rs src/keychain.rs src/ollama.rs /root/pin-client/src-tauri/target/release/build/pin-client-3586576af64bda26/out/bd9721f62ce4566f8decb25e03024c5dd0effa8b8bcd5a87e7dbfb3819d0dc06 /root/pin-client/src-tauri/../src/index.html /root/pin-client/src-tauri/target/release/build/pin-client-3586576af64bda26/out/tauri-codegen-assets/827604572db6e3546043938d905e7055476fa95058b4486dcb3475d544b29123.html
 
-/root/pin-client/src-tauri/target/release/deps/pin_client-cad7e10572065f4a: src/main.rs src/websocket.rs src/keychain.rs src/ollama.rs
+/root/pin-client/src-tauri/target/release/deps/pin_client-cad7e10572065f4a: src/main.rs src/websocket.rs src/keychain.rs src/ollama.rs /root/pin-client/src-tauri/target/release/build/pin-client-3586576af64bda26/out/bd9721f62ce4566f8decb25e03024c5dd0effa8b8bcd5a87e7dbfb3819d0dc06 /root/pin-client/src-tauri/../src/index.html /root/pin-client/src-tauri/target/release/build/pin-client-3586576af64bda26/out/tauri-codegen-assets/827604572db6e3546043938d905e7055476fa95058b4486dcb3475d544b29123.html
 
 src/main.rs:
 src/websocket.rs:
 src/keychain.rs:
 src/ollama.rs:
+/root/pin-client/src-tauri/target/release/build/pin-client-3586576af64bda26/out/bd9721f62ce4566f8decb25e03024c5dd0effa8b8bcd5a87e7dbfb3819d0dc06:
+/root/pin-client/src-tauri/../src/index.html:
+/root/pin-client/src-tauri/target/release/build/pin-client-3586576af64bda26/out/tauri-codegen-assets/827604572db6e3546043938d905e7055476fa95058b4486dcb3475d544b29123.html:
+
+# env-dep:CARGO_PKG_AUTHORS=AiAssist Secure
+# env-dep:CARGO_PKG_DESCRIPTION=PIN Client - P2P Inference Network Operator Client
+# env-dep:CARGO_PKG_NAME=pin-client
+# env-dep:OUT_DIR=/root/pin-client/src-tauri/target/release/build/pin-client-3586576af64bda26/out
